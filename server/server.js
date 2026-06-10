cb532af3f0fa675859ce6488266cc2643d162167
markmuturi <muturimark16@gmail.com>
2026-06-10 17:58:37 +0300

    Fix: Front to Back Connection

--- server/server.js
+++ server/server.js
@@ -13,7 +13,8 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "http://localhost:5000"
+        "http://localhost:5000",
+        "https://suave-studio.vercel.app/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
